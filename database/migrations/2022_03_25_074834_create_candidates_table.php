ec07e6b579a3a9b5752004b88d68581406aa2475
adipagastya <adipa.pt@gmail.com>
2022-03-27 10:45:45 +0800

    add candidate

---
+++ database/migrations/2022_03_25_074834_create_candidates_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('place_birth');
+            $table->string('date_birth');
+            $table->string('studies');
+            $table->string('major');
+            $table->string('edu_level');
+            $table->string('grad_year');
+            $table->string('grad_year');
+            $table->string('study_certificate');
+            $table->string('transcript');            
+            $table->string('transcript');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
