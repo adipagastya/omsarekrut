fdceebce9984260dfaaaf8e28d4142b404ead4f8
omsamedic <ekayudinatha@gmail.com>
2022-04-26 10:46:44 +0800

    update migration candidates and work experiences

--- database/migrations/2022_03_29_012533_create_work_experiences_table.php
+++ database/migrations/2022_03_29_012533_create_work_experiences_table.php
@@ -16,8 +16,12 @@ class CreateWorkExperiencesTable extends Migration
         Schema::create('work_experiences', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('year');
+            $table->string('position');
+            $table->string('start_year');
+            $table->string('end_year');
             $table->text('description');
+            $table->string('lead_name');
+            $table->string('lead_phone_number');
             $table->foreignId('id_candidate');
             $table->timestamps();
         });
